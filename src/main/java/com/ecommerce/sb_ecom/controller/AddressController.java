88846cdaf250cef3b741da9ef07323ce5ff6427c
MartinFerret <mferret.pro@gmail.com>
2024-11-12 14:33:31 +0100

    feat(): Addresses GET

--- src/main/java/com/ecommerce/sb_ecom/controller/AddressController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/AddressController.java
@@ -7,10 +7,9 @@ import com.ecommerce.sb_ecom.utils.AuthUtil;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -29,5 +28,11 @@ public class AddressController {
         User user = authUtil.loggedInUser();
         AddressDTO savedAddressDTO =  addressService.createAddress(addressDTO, user);
         return new ResponseEntity<>(savedAddressDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/addresses")
+    public ResponseEntity<List<AddressDTO>> getAddresses() {
+        List<AddressDTO> addressList =  addressService.getAddresses();
+        return new ResponseEntity<>(addressList, HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/sb_ecom/service/address/AddressService.java
+++ src/main/java/com/ecommerce/sb_ecom/service/address/AddressService.java
@@ -3,6 +3,10 @@ package com.ecommerce.sb_ecom.service.address;
 import com.ecommerce.sb_ecom.dto.AddressDTO;
 import com.ecommerce.sb_ecom.model.User;
 
+import java.util.List;
+
 public interface AddressService {
     AddressDTO createAddress(AddressDTO addressDTO, User user);
+
+    List<AddressDTO> getAddresses();
 }
