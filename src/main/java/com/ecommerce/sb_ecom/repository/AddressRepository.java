0f95dcdae3f11991067085ae58977351b19e3f15
MartinFerret <mferret.pro@gmail.com>
2024-11-13 12:50:00 +0100

    feat(): add annotations.

--- src/main/java/com/ecommerce/sb_ecom/repository/AddressRepository.java
+++ src/main/java/com/ecommerce/sb_ecom/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package com.ecommerce.sb_ecom.repository;
 
 import com.ecommerce.sb_ecom.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
