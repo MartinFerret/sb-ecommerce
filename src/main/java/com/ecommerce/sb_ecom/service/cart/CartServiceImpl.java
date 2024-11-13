0d395063ff689ff650f9fd2017c8f4167801f7e1
MartinFerret <mferret.pro@gmail.com>
2024-11-13 10:06:54 +0100

    fix(): fix quantity.

--- src/main/java/com/ecommerce/sb_ecom/service/cart/CartServiceImpl.java
+++ src/main/java/com/ecommerce/sb_ecom/service/cart/CartServiceImpl.java
@@ -101,7 +101,12 @@ public class CartServiceImpl implements CartService {
 
         return carts.stream().map(cart -> {
             CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
-            List<ProductDTO> products = cart.getCartItems().stream().map(p -> modelMapper.map(p.getProduct(), ProductDTO.class)).toList();
+            List<ProductDTO> products = cart.getCartItems().stream().map(cartItem -> {
+                ProductDTO productDTO = modelMapper.map(cartItem.getProduct(), ProductDTO.class);
+                productDTO.setQuantity(cartItem.getQuantity());
+                return productDTO;
+            }).toList();
+
 
             cartDTO.setProducts(products);
 
